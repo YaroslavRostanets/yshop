fd11e53c198c942387900fb21f9c968088d1fcc8
Yaroslav R <rostanets@gmail.com>
2017-07-14 21:08:13 +0300

    upd gitignore

--- layouts/menu.php
+++ layouts/menu.php
@@ -1,13 +1,17 @@
 <ul class="nav navbar-nav nav_1">
-    <li><a href="<?= SITE_ROOT ?>">Главная</a></li>
-    <li class="dropdown mega-dropdown active">
-        <a href="<?= SITE_ROOT ?>/1" class="dropdown-toggle" data-toggle="dropdown">Для женщин<span class="caret"></span></a>
+    <li>
+        <a href="<?= SITE_ROOT ?>/">Главная</a>
     </li>
-    <li class="dropdown mega-dropdown active">
-        <a href="<?= SITE_ROOT ?>/2" class="dropdown-toggle" data-toggle="dropdown">Для мужчин<span class="caret"></span></a>
+    <li>
+        <a href="<?= SITE_ROOT ?>/1">Для женщин<span class="caret"></span></a>
     </li>
-    <li><a href="<?= SITE_ROOT ?>/product">Каталог</a></li>
-    <li><a href="<?= SITE_ROOT ?>/user/login/">Вход</a></li>
-    <li class="last"><a href="<?= SITE_ROOT ?>/product/">Контакты</a></li>
-
+    <li>
+        <a href="<?= SITE_ROOT ?>/2">Для мужчин<span class="caret"></span></a>
+    </li>
+    <li>
+        <a href="<?= SITE_ROOT ?>/product">Каталог</a>
+    </li>
+    <li class="last">
+        <a href="<?= SITE_ROOT ?>/product/">Контакты</a>
+    </li>
 </ul>
